ebd4c86b83f2afb4a47ed8f8bc713f6a4e14ef85
matihaw <matihaw17@gmail.com>
2020-05-04 14:13:14 +0200

    Added functionality for other channels

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-let valrontPlayers;
-let boardgamers;
-let roles;
-let messageBoardgame = true;;
-let messageValorant = true;;
 
+
+/*
+ * Prepare Bot
+ */
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  boardgamers = Array.from(client.guilds.cache.get("539889142373548032").roles.cache.get("706544728245796876").members.keys());
-  valrontPlayers = Array.from(client.guilds.cache.get("539889142373548032").roles.cache.get("706543671792762940").members.keys());
- 
 });
 
 client.on('message', msg => {
@@ -20,71 +16,225 @@ client.on('message', msg => {
 });
 
 
-///Connecting to channel
+/*
+ * EVENT - Join/Leave channel
+ */
 client.on('voiceStateUpdate', (oldMember, newMember) => {
+  /*
+  * List of user with spec. role
+  */
+  let valrontPlayers;
+  let boardgamePlayers;
+  let LeaugeOfLegendsPlayers;
+  let watch2getherPlayers;
+  /*
+   * Turning message on
+   */
+  let messageBoardgame = true;
+  let messageValorant = true;
+  let messageLeaugeOfLegends = true;
+  let messageWatch2Gether = true;
+  /*
+   *  Id of newMember channel
+   */
   let newUserChannel = newMember.channelID
 
-  ///boardgames
+  /*
+   * Channel - BOARDGAME; id - 706525403992293398
+   */
   if(newUserChannel === "706525403992293398") {
-    ///get boardgame channel amount of current users
+    /*
+     * Assing variables to list of users
+     */
+    boardgamePlayers = Array.from(client.guilds.cache.get("539889142373548032").roles.cache.get("706544728245796876").members.keys());
+    /*
+     * Amount of current in spec. channel
+     */
     client.channels.fetch('706525403992293398')
     .then(channel => {
-      ///if amount of user = 1, then message all boardgames user with message
+      /*
+       * Channel size: 1 -> send message, then turn of sennding messages
+       */
       if(channel.members.size === 1){
         if(messageBoardgame){
-          console.log("message ON truning to off")
+
           let userName = channel.members.values().next().value.user.username;
-          boardgamers.forEach(userId =>{
-            
-            client.users.cache.get(userId).send(` ${userName} zaprasza na planszówkę`);
-            messageBoardgame = false;
-            
+          boardgamePlayers.forEach(userId =>{
+            if(client.users.cache.get(userId).presence.status !== 'offline'){
+              if(userId !==  channel.members.values().next().value.user.id){
+              /*
+               * Sending messages to other user of same role
+               */
+              client.users.cache.get(userId).send(` ${userName} zaprasza na planszówkę`);
+              }
+              
+            }
           });
+          /*
+               * MESSAGE TURNED OF
+               */
+          messageBoardgame = false;
         }
       }
     });
-    ///if channel is empty messages are turned on
+    /*
+     * Channnel size: 0 -> turn on sending messages
+     */
   } if(newMember.voiceChannel === undefined  && oldMember.channelID ==='706525403992293398'){
-    client.channels.fetch('706525403992293398')
-    .then(channel => {
-      ///if amount of user = 1, then message all boardgames user with message  
-      if(channel.members.size === 0){
-        console.log("boardgame message ON")
-        messageBoardgame = true;
-      }
-    });
+    messageBoardgame = turnOnMessage('706525403992293398');
   }
 
-
+  /*
+   * Channel - VALORANT; id - 706525496996921406
+   */
    if(newUserChannel === '706525496996921406'){
+     /*
+      * Assing variables to list of users
+      */
+     valrontPlayers = Array.from(client.guilds.cache.get("539889142373548032").roles.cache.get("706543671792762940").members.keys());
+     /*
+      * Amount of current in spec. channel
+      */
     client.channels.fetch('706525496996921406')
     .then(channel => {
+      /*
+       * Channel size: 1 -> send message, then turn of sennding messages
+       */
       if(channel.members.size === 1){
         if(messageValorant){
-          console.log("Valront off")
           let userName = channel.members.values().next().value.user.username;
           valrontPlayers.forEach(userId =>{
-          
-            client.users.cache.get(userId).send(` ${userName} zaprasza na grę w Valorant'a`);
-            messageValorant = false;
-            
+            if(client.users.cache.get(userId).presence.status !== 'offline'){
+              if(userId !==  channel.members.values().next().value.user.id){
+               /*
+                * Sending messages to other user of same role
+                */
+                client.users.cache.get(userId).send(` ${userName} zaprasza na grę w Valorant'a`);
+              }
+            }
           });
+          /*
+           * MESSAGE TURNED OF
+           */
+          messageValorant = false;
         }
       }
     });
+    /*
+     * Channnel size: 0 -> turn on sending messages
+     */
   } if(newMember.voiceChannel === undefined  && oldMember.channelID ==='706525496996921406'){
-    client.channels.fetch('706525496996921406')
-    .then(channel => {
-      ///if amount of user = 1, then message all boardgames user with message  
-      if(channel.members.size === 0){
-        console.log("valornt on")
-        messageValorant = true;
-      }
-    });
+    messageValorant = turnOnMessage('706525496996921406');
   }
 
 
-})
+/*
+   * Channel - LEAUGE OF LEGENDS; id - 706526177929724245
+   */
+  if(newUserChannel === '706526177929724245'){
+    /*
+     * Assing variables to list of users
+     */
+    LeaugeOfLegendsPlayers = Array.from(client.guilds.cache.get("539889142373548032").roles.cache.get("706544637988438096").members.keys());
+    /*
+     * Amount of current in spec. channel
+     */
+   client.channels.fetch('706526177929724245')
+   .then(channel => {
+     /*
+      * Channel size: 1 -> send message, then turn of sennding messages
+      */
+     if(channel.members.size === 1){
+       if(messageLeaugeOfLegends){
+         let userName = channel.members.values().next().value.user.username;
+         LeaugeOfLegendsPlayers.forEach(userId =>{
+           if(client.users.cache.get(userId).presence.status !== 'offline'){
+            if(userId !==  channel.members.values().next().value.user.id){
+             /*
+              * MESSAGE TURNED OF
+              */
+              client.users.cache.get(userId).send(` ${userName} zaprasza na grę w LOL'a`);
+            }
+           }
+         });
+         /*
+          *   MESSAGE TURNED OF
+          */
+         messageLeaugeOfLegends = false;
+       }
+     }
+   });
+   /*
+    * Channnel size: 0 -> turn on sending messages
+    */
+ } if(newMember.voiceChannel === undefined  && oldMember.channelID ==='706526177929724245'){
+  messageLeaugeOfLegends = turnOnMessage('706526177929724245');
+ }
+
+
+  /*
+   * Channel - Watch2Gether; id - 706546210902900737
+   */
+  if(newUserChannel === '706546210902900737'){
+    /*
+     * Assing variables to list of users
+     */
+    watch2getherPlayers = Array.from(client.guilds.cache.get("539889142373548032").roles.cache.get("706544843379441754").members.keys());
+    /*
+     * Amount of current in spec. channel
+     */
+   client.channels.fetch('706546210902900737')
+   .then(channel => {
+     /*
+      * Channel size: 1 -> send message, then turn of sennding messages
+      */
+     if(channel.members.size === 1){
+       if(messageWatch2Gether){
+         let userName = channel.members.values().next().value.user.username;
+         watch2getherPlayers.forEach(userId =>{
+           if(client.users.cache.get(userId).presence.status !== 'offline'){
+             /*
+              * Sending message
+              */
+             client.users.cache.get(userId).send(` HawryloBot zaprasza na oglondanko-> https://www.watch2gether.com/rooms/oglondanko-urevlabm6dk8mwtcf5?lang=pl <-`);
+
+           }
+         });
+         /*
+          *   MESSAGE TURNED OF
+          */
+         messageWatch2Gether = false;
+       }
+     }
+   });
+   /*
+    * Channnel size: 0 -> turn on sending messages
+    */
+ } if(newMember.voiceChannel === undefined  && oldMember.channelID ==='706546210902900737'){
+  messageWatch2Gether = turnOnMessage('706546210902900737');
+ }
+
+ 
+/*
+ * END OF LISTENER
+ */
+});
+
+/*
+ * Function to turn off messages for spec channel (DRY) 
+ */
+function turnOnMessage(channelId){
+  client.channels.fetch(channelId)
+  .then(channel => {
+    /*
+     *  Channel size: 0 -> turning messages ON for /'channel'/
+     */
+    if(channel.members.size === 0){
+      return true;
+    }
+  });
+}
+
 
 
 
